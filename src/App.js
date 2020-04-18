3f41f0008fe9ef2cb30640cd7b7cc1b81d46a3c6
GuiAndrew <guiandrew.pt@gmail.com>
2020-04-18 12:14:16 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import DropdownMenu from "./components/DropDownMenu/dropDown"
+import "bootstrap/dist/css/bootstrap.min.css";
+import DropdownMenu from "./components/DropDownMenu/dropDown";
 
+class App extends Component {
+  state = {
+    colorText: "black",
+    backgroundColor: "white",
+  };
 
-function App() {
-  return (
-    <>
-  <div className="App">Started Page</div>
-  <DropdownMenu/>
-  </>
-  )}
+  handleColors = (hex, isText) => {
+    if (isText) {
+      this.setState({ colorText: hex });
+    } else {
+      this.setState({ backgroundColor: hex });
+    }
+  };
+
+  render() {
+    const { colorText, backgroundColor } = this.state;
+    return (
+      <>
+        <DropdownMenu
+          handleColors={this.handleColors(colorText, backgroundColor)}
+        />
+        <div style={{ colorText, backgroundColor }}></div>
+      </>
+    );
+  }
+}
 
 export default App;
