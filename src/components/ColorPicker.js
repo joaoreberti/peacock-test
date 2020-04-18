69cf3c2c5a9e88bd3efe7ec37021ff42f81bd8ae
andre3992 <andre.regedor@hotmail.com>
2020-04-18 12:17:22 +0100

    dropDown

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { ChromePicker } from 'react-color';
 
 class ColorPicker extends React.Component {
-  state = {
-    background: '#fff',
-     disableAlpha: false,
-   };
+    state = {
+        background: '#fff',
+        disableAlpha: false,
+    };
 
-  handleChangeComplete = (color) => {
-    this.setState({ background: color.hex });
-  };
+    handleChangeComplete = (color) => {
+        this.setState({ background: color.hex });
+    };
 
-  render() {
-    return (
-      <ChromePicker
-        color={ this.state.background }
-        onChangeComplete={ this.handleChangeComplete }
-      />
-    );
-  }
+    render() {
+        return ( <
+            ChromePicker color = { this.state.background }
+            onChangeComplete = { this.handleChangeComplete }
+            />
+        );
+    }
 }
 
 export default ColorPicker
