361b67bc73b710e1d39a45e8e8f0178702956bfb
andre3992 <andre.regedor@hotmail.com>
2020-04-18 12:30:56 +0100

    changes to color picker and dropdown

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -2,24 +2,6 @@ import React from 'react';
 import { ChromePicker } from 'react-color';
 
 class ColorPicker extends React.Component {
-<<<<<<< HEAD
-    state = {
-        background: '#fff',
-        disableAlpha: false,
-    };
-
-    handleChangeComplete = (color) => {
-        this.setState({ background: color.hex });
-    };
-
-    render() {
-        return ( <
-            ChromePicker color = { this.state.background }
-            onChangeComplete = { this.handleChangeComplete }
-            />
-        );
-    }
-=======
   state = {
     background: '#fff',
      disableAlpha: false,
@@ -38,16 +20,14 @@ class ColorPicker extends React.Component {
   render() {
     return (
         <div>
-            {this.state.visibile && <ChromePicker
+            {this.props.visibile && <ChromePicker
         color={ this.state.background }
         onChangeComplete={ this.handleChangeComplete }
       />}
-      <button onClick={this.save}>Save</button>
-      
+      {/* <button onClick={this.save}>Save</button> */}
       </div>
     );
   }
->>>>>>> bedd31863d5a3273fba762f835cc6474953fd1d3
 }
 
 export default ColorPicker
